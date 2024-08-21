d0e163051a584fd870671ef81694c8191c77de58
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-22 00:48:42 +0100

    feat: Enhance app setup with session management and routing
    - Configured 'dotenv' for environment variables.
    - Added 'express-ejs-layouts' for layout management.
    - Integrated 'express-session' with 'connect-pg-simple' for PostgreSQL session storage.
    - Set up HTTP server and updated port handling.
    - Included routing for index and messages.
    - Updated middleware for session management and view engine.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,73 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const createError = require("http-errors");
 const path = require("path");
-const logger = require("morgan");
+const expressLayouts = require("express-ejs-layouts");
+const indexRouter = require("./routes/indexRouter");
+const messagesRouter = require("./routes/messagesRouter");
+const debug = require("debug")("mini-:members-only:server");
+const http = require("http");
+const expressSession = require("express-session");
+const pgSession = require("connect-pg-simple")(expressSession);
+const pgPool = require("./db/index");
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = process.env.PORT || "3000";
+app.set("port", port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port, () => {
+  debug("Listening on " + port);
+});
 
 /**
 ----------------------SET UP THE VIEW ENGINE------------------------
 */
-app.set("views", paths.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(expressLayouts);
 
 /**
 ----------------------USE MIDDLEWARE FUNCTIONS------------------------
 */
 app.use(express.json()); // sets `Content-type `to `text/plain`it Acept header doesn't contain `applicaiton/json`
 app.use(express.static(path.join(__dirname, "public"))); // sets the directory from which static files will be served
+
+/**
+----------------------USE MIDDLEWARE FUNCTIONS------------------------
+*/
+
+// Set up the express-session middleware to store user sessions
+app.use(
+  expressSession({
+    store: new pgSession({
+      pool: pgPool, // connection pool to PostgreSQL db
+      tableName: "user_sessions",
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 10 * 24 * 60 * 60 * 1000 },
+  }),
+);
+
+/**
+----------------------ROUTES------------------------
+*/
+app.use("/", indexRouter);
+app.use("/messages", messagesRouter);
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -26,3 +79,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+module.exports = app;
