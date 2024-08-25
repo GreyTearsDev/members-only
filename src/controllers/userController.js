c30662f7241540c8fe38a65bbb3813f20bef5f92
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-25 15:50:13 +0100

    feat: Define handle function for GET and POST sign-up requests
    - Required 'body' and 'validationResult' functions from the 'express-validator' library
    - Required query functions from 'queries.js' to handle database manipulation
    - Required the 'express-async-handler' to streamline error handling in async functions
    - Defined and exported 'sign_up_get for rendering the Sign Up form
    - Defined and exported 'sign_up_post for processing Sign Up form data
    ---- Used the methods from 'express-validator' to sanitize and validate the data
    ---- Define an anonymous function that rerenders the form when errors occur and, when they don't, saves the user info to the database

---
+++ src/controllers/userController.js
@@ -0,0 +1,75 @@
+const { body, validationResult } = require("express-validator");
+const db = require("../db/queries");
+const asyncHandler = require("express-async-handler");
+
+// handler for displaying the sign-up form to the user
+exports.sign_up_get = (req, res, next) => {
+  res.render("sign-up-form", {
+    title: "Sign up",
+    errors: undefined,
+    username: undefined,
+    first_name: undefined,
+    last_name: undefined,
+    password: undefined,
+  });
+};
+
+// handler for validating and adding user to the datatbase
+exports.sign_up_post = [
+  body("username", "You must create a username").trim().notEmpty().escape(),
+  body("first_name", "You must add a first name").trim().notEmpty().escape(),
+  body("last_name", "You must add a last name").trim().notEmpty().escape(),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("You must add a password")
+    .isLength({ min: 3 })
+    .withMessage("Passwords should be at least 3 characters long")
+    .escape(),
+  body("password_confirmation")
+    .trim()
+    .escape()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords do not match");
+      }
+      return true;
+    }),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const user = {
+      username: req.body.username,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      password: req.body.password,
+    };
+
+    if (!errors.isEmpty()) {
+      res.render("sign-up-form", {
+        title: "Sign up",
+        errors: errors.array(),
+        username: user.username,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        password: user.password,
+      });
+      return;
+    }
+
+    // Check if a user with the same username already exists
+    const existingUser = await db.getUserByUsername(user.username);
+
+    if (existingUser) {
+      const errMessage = `Username '${user.username}' is already taken. Try another one.`;
+      res.render("sign-up-form", {
+        title: "Sign up",
+        errors: [{ msg: errMessage }],
+        user: user,
+      });
+      return;
+    }
+
+    await db.insertUser(user);
+    res.redirect("/");
+  }),
+];
