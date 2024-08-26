77a059cd1590f606f62418751f496d14c0eb22d1
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-26 09:00:22 +0100

    feat: Add user routes for sign-up and log-in
    - Implemented routes for user sign-up and log-in.
    - Connected routes to corresponding controller actions for handling user registration and authentication.

---
+++ src/routes/userRouter.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+router.get("/sign-up", userController.sign_up_get);
+router.post("/sign-up", userController.sign_up_post);
+router.get("/log-in", userController.log_in_get);
+router.post("/log-in", userController.log_in_post);
+module.exports = router;
