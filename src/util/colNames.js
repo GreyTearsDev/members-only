efb6b0b7c1bcc033f08205cf1ab8138e044bd4f3
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-28 00:15:06 +0100

    feat: Add constants for admin and member passwords

--- src/util/colNames.js
+++ src/util/colNames.js
@@ -2,10 +2,14 @@ const ID = "id";
 const USERNAME = "username";
 const IS_ADMIN = "is_admin";
 const IS_MEMBER = "is_member";
+const PASS_ADMIN = "admin";
+const PASS_MEMBER = "member";
 
 module.exports = {
   ID,
   USERNAME,
   IS_ADMIN,
   IS_MEMBER,
+  PASS_ADMIN,
+  PASS_MEMBER,
 };
