ea88b5983c13d7b816d7328a6cfa61f87ee518d8
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-27 18:34:16 +0100

    feat: Add GET and POST routes to handle previlege assignment in userRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -7,5 +7,7 @@ router.post("/sign-up", userController.sign_up_post);
 router.get("/log-in", userController.log_in_get);
 router.post("/log-in", userController.log_in_post);
 router.get("/log-out", userController.log_out_get);
+router.get("/previleges", userController.previleges_get);
+router.post("/previleges", userController.previleges_post);
 
 module.exports = router;
