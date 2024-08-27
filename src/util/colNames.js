3e4b1a625d128042176b61cb69ccf564dff6cf05
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-27 21:50:57 +0100

    feat: Add admin and member status column names to util module

--- src/util/colNames.js
+++ src/util/colNames.js
@@ -1,7 +1,11 @@
 const ID = "id";
 const USERNAME = "username";
+const IS_ADMIN = "is_admin";
+const IS_MEMBER = "is_member";
 
 module.exports = {
   ID,
   USERNAME,
+  IS_ADMIN,
+  IS_MEMBER,
 };
