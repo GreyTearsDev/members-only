a746ea723acd14cae2dc8668805e8f01a10e40e4
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-29 15:19:03 +0100

    refactor: Simplify layout using custom Tailwind grid templates
    - Removed inline styles and complex class definitions from 'layout.ejs' in favor of custom Tailwind CSS grid templates.
    - Updated 'tailwind.config.js' to extend the theme with 'gridTemplateRows' and 'gridTemplateColumns' for a structured and responsive layout.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   content: ["./src/views/**/*.{html, js}", "./public/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        "custom-layout": "80px 1fr 50px",
+      },
+      gridTemplateColumns: {
+        "custom-layout": "minmax(100px, 250px) 1fr",
+      },
+    },
   },
   variants: {
     extend: {},
