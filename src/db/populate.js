791076da041a570e6dceca800c97e66e46058b3a
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-08-31 09:08:58 +0100

    feat: Add SSL connection support for database population script

--- src/db/populate.js
+++ src/db/populate.js
@@ -64,6 +64,7 @@ async function main() {
   );
   const pool = new Pool({
     connectionString: argv[2],
+    ssl: argv[2] ? true : false,
   });
   console.log("---Connected--- \n*__________________________________*");
 
